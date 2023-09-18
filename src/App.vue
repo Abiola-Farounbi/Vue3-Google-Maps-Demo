323395ee8271ad6731d7e4b8928dd64ae248ce3b
Abiola-Farounbi <farounbiabiola@gmail.com>
2023-09-18 18:39:01 +0100

    Add project files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <GMapMap
+   :center="center"
+   :zoom="7"
+   map-type-id="terrain"
+   style="width: 1000px; height: 800px"
+  >
+  </GMapMap>
+ </template>
+ 
+ <script>
+  export default {
+   name: "App",
+   data() {
+    return {
+     center: { lat: 51.093048, lng: 6.84212 },
+    };
+   },
+  };
+ </script>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+ //Imports the map package
+import VueGoogleMaps from "@fawmi/vue-google-maps";
+
+const app = createApp(App);
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+app.use(VueGoogleMaps, {
+  load: {
+    key: GOOGLE_MAPS_API_KEY,
+  },
+ })
+ .mount("#app");
+
+
